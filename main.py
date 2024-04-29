28d026205cf323f453df41761fb552d400247ae8
Ivo de Jong <ivopascal@gmail.com>
2024-04-29 13:21:05 +0200

    ood detection now has leave-one-out and roc curves

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 
 from disentanglement.benchmarks.decreasing_dataset import plot_decreasing_dataset
 from disentanglement.benchmarks.label_noise import label_noise
-from disentanglement.benchmarks.ood_class_detection import ood_class_detection
+from disentanglement.benchmarks.ood_class_detection import plot_ood_class_detection
 from disentanglement.data.datasets import get_datasets
 from disentanglement.settings import TEST_MODE
 
@@ -17,12 +17,12 @@ def main():
                 plot_decreasing_dataset(name, conf)
                 label_noise(name, conf)
             if name == "CIFAR10":
-                ood_class_detection(name, conf)
+                plot_ood_class_detection(name, conf)
 
         else:
             plot_decreasing_dataset(name, conf)
             label_noise(name, conf)
-            ood_class_detection(name, conf)
+            plot_ood_class_detection(name, conf)
     end_time = datetime.now()
 
     print(f"Ran all experiments in {end_time - start_time}")

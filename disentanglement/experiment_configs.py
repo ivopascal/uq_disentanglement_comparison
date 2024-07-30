bb12ff75effe085f7637d62e146a0de9fd40fb6a
Ivo de Jong <ivopascal@gmail.com>
2024-07-30 17:09:37 +0200

    enable all runs

--- disentanglement/experiment_configs.py
+++ disentanglement/experiment_configs.py
@@ -212,34 +212,34 @@ def get_experiment_configs() -> List[ExperimentConfig]:
         return get_test_mode_configs()
 
     return [
-        # *[get_cifar10_config(meta_experiments=["decreasing_dataset",
-        #                                        "label_noise",
-        #                                        "ood_class"
-        #                                        ], run_index=i) for i in range(N_CIFAR_REPETITIONS)]
-        # ,
-        # get_cifar10_plotting_config(["decreasing_dataset",
-        #                              "label_noise",
-        #                              "ood_class"]),
-        # get_blobs_config(meta_experiments=["decreasing_dataset",
-        #                                    "label_noise",
-        #                                    ]),
-        # *get_eeg_configs(meta_experiments=["decreasing_dataset",
-        #                                    "label_noise",
-        #                                    "ood_class"
-        #                                    ]),
-        # get_eeg_plotting_config(meta_experiments=["decreasing_dataset",
-        #                                           "label_noise",
-        #                                           "ood_class"
-        #                                           ])
-        # *[get_fashion_mnist_configs(meta_experiments=["decreasing_dataset",
-        #                                               "label_noise",
-        #                                               "ood_class"
-        #                                               ], run_index=i) for i in range(N_CIFAR_REPETITIONS)][::-1],
-        # get_fashion_mnist_plotting_config(["decreasing_dataset",
-        #                                    "label_noise",
-        #                                    "ood_class"])
-        *[get_wine_config(meta_experiments=[# "decreasing_dataset",
-                                            # "label_noise",
+        *[get_cifar10_config(meta_experiments=["decreasing_dataset",
+                                               "label_noise",
+                                               "ood_class"
+                                               ], run_index=i) for i in range(N_CIFAR_REPETITIONS)]
+        ,
+        get_cifar10_plotting_config(["decreasing_dataset",
+                                     "label_noise",
+                                     "ood_class"]),
+        get_blobs_config(meta_experiments=["decreasing_dataset",
+                                           "label_noise",
+                                           ]),
+        *get_eeg_configs(meta_experiments=["decreasing_dataset",
+                                           "label_noise",
+                                           "ood_class"
+                                           ]),
+        get_eeg_plotting_config(meta_experiments=["decreasing_dataset",
+                                                  "label_noise",
+                                                  "ood_class"
+                                                  ]),
+        *[get_fashion_mnist_configs(meta_experiments=["decreasing_dataset",
+                                                      "label_noise",
+                                                      "ood_class"
+                                                      ], run_index=i) for i in range(N_CIFAR_REPETITIONS)][::-1],
+        get_fashion_mnist_plotting_config(["decreasing_dataset",
+                                           "label_noise",
+                                           "ood_class"]),
+        *[get_wine_config(meta_experiments=["decreasing_dataset",
+                                            "label_noise",
                                             "ood_class"
                                             ], run_index=i) for i in range(N_CIFAR_REPETITIONS)][::-1],
         get_wine_plotting_config(["decreasing_dataset",

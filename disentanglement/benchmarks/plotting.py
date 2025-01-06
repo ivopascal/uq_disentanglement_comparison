492cc29a562af6bdf706ac71c6dc8f1307e3885d
Ivo de Jong <ivopascal@gmail.com>
2025-01-06 15:31:24 +0100

    order funcs by importance

--- disentanglement/benchmarks/plotting.py
+++ disentanglement/benchmarks/plotting.py
@@ -5,22 +5,6 @@ from matplotlib import pyplot as plt
 from disentanglement.datatypes import UncertaintyResults
 
 
-def plot_confidence_intervals(ax, results, std, get_string, label, color=None):
-    uncertainties_mean = np.array(results.__getattribute__(get_string))
-    uncertainties_std = np.array(std.__getattribute__(get_string))
-    ax.fill_between(results.changed_parameter_values,
-                    uncertainties_mean - 1.96 * uncertainties_std,
-                    uncertainties_mean + 1.96 * uncertainties_std,
-                    alpha=0.3, label=label, color=color)
-
-
-def plot_line_and_confidence_interval(ax, results, std, get_string, label, color=None):
-    ax.plot(results.changed_parameter_values, results.__getattribute__(get_string), label=label, color=color)
-
-    if std:
-        plot_confidence_intervals(ax, results, std, get_string, label=f"_{label}", color=color)
-
-
 def plot_ale_epi_acc_on_axes(ax, results: UncertaintyResults, accuracy_y_ax_to_share=None, is_final_column=False,
                              std=None):
     if not results:
@@ -41,3 +25,21 @@ def plot_ale_epi_acc_on_axes(ax, results: UncertaintyResults, accuracy_y_ax_to_s
         accuracy_axes.sharey(accuracy_y_ax_to_share)
 
     return accuracy_axes
+
+
+def plot_line_and_confidence_interval(ax, results, std, get_string, label, color=None):
+    ax.plot(results.changed_parameter_values, results.__getattribute__(get_string), label=label, color=color)
+
+    if std:
+        plot_confidence_intervals(ax, results, std, get_string, label=f"_{label}", color=color)
+
+
+def plot_confidence_intervals(ax, results, std, get_string, label, color=None):
+    uncertainties_mean = np.array(results.__getattribute__(get_string))
+    uncertainties_std = np.array(std.__getattribute__(get_string))
+    ax.fill_between(results.changed_parameter_values,
+                    uncertainties_mean - 1.96 * uncertainties_std,
+                    uncertainties_mean + 1.96 * uncertainties_std,
+                    alpha=0.3, label=label, color=color)
+
+
